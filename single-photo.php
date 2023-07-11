a2a85f93fbadaf75bf28754105596d5fac7995dc
Noirbenne Valentin <valentin.noirbenne@laposte.net>
2023-07-11 21:54:28 +0200

    Modification en liste des éléments

--- single-photo.php
+++ single-photo.php
@@ -1,18 +1,19 @@
 <?php get_header(); ?>
+
   <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    
     <article class="post">
       <?php the_post_thumbnail(); ?>
 
       <h1><?php the_title(); ?></h1>
 
       <div class="post__meta">
-        <p>
-          Référence : <?php the_field( 'reference' ); ?><br>
-          Catégorie : <?php the_terms( $post->ID, 'categorie'); ?><br>
-          Format : <?php the_terms( $post->ID, 'format'); ?><br>
-          Type : <?php the_field( 'type' ); ?><br>
-          Année : <?php the_terms( $post->ID, 'annee'); ?>
+          <ul class="desc">
+            <li id="ref" data-reference="<?php echo esc_attr( get_field( 'reference' ) ); ?>">Référence : <?php the_field( 'reference' ); ?></li>
+            <li>Catégorie : <?php the_terms( $post->ID, 'categorie'); ?></li>
+            <li>Format : <?php the_terms( $post->ID, 'format'); ?></li>
+            <li>Type : <?php the_field( 'type' ); ?></li>
+            <li>Année : <?php the_terms( $post->ID, 'annee'); ?></li>
+          </ul>
         </p>
       </div>
 
@@ -20,6 +21,6 @@
         <?php the_content(); ?>
       </div>
     </article>
-
   <?php endwhile; endif; ?>
+
 <?php get_footer(); ?>

5001007c359992e555c590ec8acadd1724ff2eae
Noirbenne Valentin <valentin.noirbenne@laposte.net>
2023-07-11 14:27:28 +0200

    Paramétrage de base du theme

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function theme_register_menus() {
+function motaphoto_register_menus() {
     register_nav_menus(
         array(
             'main-menu' 	=> __( 'Menu principal', 'motaphoto' ),
@@ -8,19 +8,45 @@ function theme_register_menus() {
         )
     );
 }
-add_action('init', 'theme_register_menus');
+add_action('init', 'motaphoto_register_menus');
 
-
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
+// Déclaration des styles
+function motaphoto_enqueue_styles() {
     wp_enqueue_style( 'theme_style', get_template_directory_uri() . '/assets/css/theme.css');
 }
+add_action( 'wp_enqueue_scripts', 'motaphoto_enqueue_styles' );
 
-function theme_setup() {
-    add_theme_support('custom-logo');
+// Déclaration des scripts
+function motaphoto_enqueue_scripts() {
+    wp_enqueue_script( 'scripts-js', get_template_directory_uri() . '/assets/js/scripts.js', array(), 1.0, true);
 }
-add_action('after_setup_theme', 'theme_setup');
+add_action( 'wp_enqueue_scripts', 'motaphoto_enqueue_scripts' );
 
 
-// Inclut le fichier menus.php
-require_once get_template_directory() . '/menus.php';
+function motaphoto_setup() {
+    // Ajouter la prise en charge des logos
+    add_theme_support('custom-logo');
+    // Ajouter la prise en charge des images mises en avant
+    add_theme_support( 'post-thumbnails' );
+    // Ajouter automatiquement le titre du site dans l'en-tête du site
+    add_theme_support( 'title-tag' );
+    get_template_part( 'menus' );
+}
+add_action('after_setup_theme', 'motaphoto_setup');
+
+
+//Fonction pour ajouter le bouton Contact pour la modale
+function contact_btn( $items, $args ) {
+    if ( $args->theme_location === 'main-menu' ) {
+        $items .= '<a href="#id01" class="contact-btn menu-item">Contact</a>';
+    }
+    return $items;
+}
+
+add_filter( 'wp_nav_menu_items', 'contact_btn', 10, 2 );
+
+
+
+
+
+
